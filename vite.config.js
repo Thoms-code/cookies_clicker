ab2d4b3ef727669f6b9425eb17d08b7764446d34
Thoms-code <dev@thomscode.dev>
2023-01-13 13:35:21 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
         }
-    }
+    },
     server: {
         port: 8080,
         hot: true
